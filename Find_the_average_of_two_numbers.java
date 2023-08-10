8131f3c521be25c0e830a21c29198b7c30661d76
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-08-10 11:58:44 +0530

    import java.util.*; public class numbers{     public static void main(String tarun[])     {         int N,M;         double A;         Scanner sc=new Scanner(System.in);         N=sc.nextInt();         M=sc.nextInt();         A=(double)(N+M)/2;         System.out.printf("%.4f",A);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class numbers{
+    public static void main(String tarun[])
+    {
+        int N,M;
+        double A;
+        Scanner sc=new Scanner(System.in);
+        N=sc.nextInt();
+        M=sc.nextInt();
+        A=(double)(N+M)/2;
+        System.out.printf("%.4f",A);
+    }
+}
