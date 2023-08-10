754c2adf47e8af93deee7475d656a34dafa67a0e
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-08-10 12:14:29 +0530

    import java.util.*; public class divi{     public static void main(String tarun[]){         int a,b,c;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         c=a/b;         System.out.printf("%d",c);     } }

---
+++ Division.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class divi{
+    public static void main(String tarun[]){
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=a/b;
+        System.out.printf("%d",c);
+    }
+}
