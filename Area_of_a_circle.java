23e72d171e6708ec601e2736c60d1d11ed81ae1e
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-08-10 11:15:06 +0530

    import java.util.*; public class king{     public static void main(String tarun[])     {         Scanner sc = new Scanner(System.in);         int r;         double A;
            r=sc.nextInt();
            A=3.14*r*r;
            System.out.printf("%.2f",A);
        }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class king{
+    public static void main(String tarun[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int r;
+        double A;
+        
+        r=sc.nextInt();
+        A=3.14*r*r;
+        System.out.printf("%.2f",A);
+    }
+}
