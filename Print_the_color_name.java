db7a35b2b1c630e2a8e6894a8582c6d227d1a266
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-08-17 11:35:58 +0530

    import java.util.*; public class tarun{     public static void main(String args[]){         Scanner V=new Scanner(System.in);         char ch = V.next().charAt(0);         if(ch=='V'){             System.out.println("Violet");         }         else if(ch=='I'){             System.out.println("Indigo");         }         else if(ch=='B'){             System.out.println("Blue");         }         else if(ch=='G'){             System.out.println("Green");         }         else if(ch=='Y'){             System.out.println("Yellow");         }         else if(ch=='O'){             System.out.println("Orange");         }         else if(ch=='R'){             System.out.println("Red");         }         else{             System.out.println("-1");         }      } }

---
+++ Print_the_color_name.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class tarun{
+    public static void main(String args[]){
+        Scanner V=new Scanner(System.in);
+        char ch = V.next().charAt(0);
+        if(ch=='V'){
+            System.out.println("Violet");
+        }
+        else if(ch=='I'){
+            System.out.println("Indigo");
+        }
+        else if(ch=='B'){
+            System.out.println("Blue");
+        }
+        else if(ch=='G'){
+            System.out.println("Green");
+        }
+        else if(ch=='Y'){
+            System.out.println("Yellow");
+        }
+        else if(ch=='O'){
+            System.out.println("Orange");
+        }
+        else if(ch=='R'){
+            System.out.println("Red");
+        }
+        else{
+            System.out.println("-1");
+        }
+     }
+}
