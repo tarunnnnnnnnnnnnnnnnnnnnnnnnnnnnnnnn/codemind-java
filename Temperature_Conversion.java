9fec2703622c3a1d5404c0df2b05f8fc6eb93e32
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-08-17 10:30:57 +0530

    import java.util.*; public class tarun{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         double s;         s=32+(n*1.8);         System.out.printf("%.2f",s);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class tarun{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        double s;
+        s=32+(n*1.8);
+        System.out.printf("%.2f",s);
+    }
+}
