c56a4795736ffd78b4a6408c8eae74e5787116c0
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-08-18 12:12:36 +0530

    import java.util.*; public class tarun{     public static void main(String[] args){         Scanner V=new Scanner(System.in);         int a,b,c;         a=V.nextInt();         b=V.nextInt();         c=a+b;         System.out.printf("%d",c);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class tarun{
+    public static void main(String[] args){
+        Scanner V=new Scanner(System.in);
+        int a,b,c;
+        a=V.nextInt();
+        b=V.nextInt();
+        c=a+b;
+        System.out.printf("%d",c);
+    }
+}
