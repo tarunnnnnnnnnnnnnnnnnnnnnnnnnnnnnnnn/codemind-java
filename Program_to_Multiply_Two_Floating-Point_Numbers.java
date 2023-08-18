a2d3237793e00ab71c9a8dbb85f46ce02443c689
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-08-18 12:16:43 +0530

    import java.util.*; public class tarun{     public static void main(String[] args){         Scanner V=new Scanner(System.in);         float a,b,c;         a=V.nextFloat();         b=V.nextFloat();         c=a*b;         System.out.printf("%.2f",c);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class tarun{
+    public static void main(String[] args){
+        Scanner V=new Scanner(System.in);
+        float a,b,c;
+        a=V.nextFloat();
+        b=V.nextFloat();
+        c=a*b;
+        System.out.printf("%.2f",c);
+    }
+}
