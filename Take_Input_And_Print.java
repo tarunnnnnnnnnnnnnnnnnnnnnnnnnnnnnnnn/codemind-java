ba54a5c641319b5fa433c6b763b917603fda8de4
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-08-19 12:05:08 +0530

    import java.util.*; public class tarun{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String V=sc.nextLine();         System.out.println(V);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class tarun{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String V=sc.nextLine();
+        System.out.println(V);
+    }
+}
