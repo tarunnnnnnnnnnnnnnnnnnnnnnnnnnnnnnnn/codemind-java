61d3cb3387697ebc628910074026ea66d8dee819
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-08-19 12:14:23 +0530

    import java.util.*; public class tarun{     public static void main(String[] args){         Scanner V=new Scanner(System.in);         int t=V.nextInt();         int s=V.nextInt();         int b=V.nextInt();         int T=t*s*b*512*2;         System.out.println(T);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class tarun{
+    public static void main(String[] args){
+        Scanner V=new Scanner(System.in);
+        int t=V.nextInt();
+        int s=V.nextInt();
+        int b=V.nextInt();
+        int T=t*s*b*512*2;
+        System.out.println(T);
+    }
+}
