30c39a7ac173f191d790ccf9bb92c7a1c046db24
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-09-08 19:47:08 +0530

    import java.util.*; public class Tarun{     public static void main(String[] args){         Scanner V=new Scanner(System.in);         char a;         a=V.next().charAt(0);         int n= a;         System.out.print(n);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Tarun{
+    public static void main(String[] args){
+        Scanner V=new Scanner(System.in);
+        char a;
+        a=V.next().charAt(0);
+        int n= a;
+        System.out.print(n);
+    }
+}
