5f3f02b11b869916751f78438b13a0f719e4b8d0
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-09-08 20:22:22 +0530

    import java.util.*; public class Tarun{     public static void main(String[] args){         Scanner V=new Scanner(System.in);         int n;         n=V.nextInt();         System.out.println(n*(n-1)/2);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Tarun{
+    public static void main(String[] args){
+        Scanner V=new Scanner(System.in);
+        int n;
+        n=V.nextInt();
+        System.out.println(n*(n-1)/2);
+    }
+}
